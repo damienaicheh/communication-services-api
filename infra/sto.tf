fbabdc842f153df0eb45ac61e68287ee223eb3e6
Divya Ghai <divyaghai@ymail.com>
2025-04-28 21:02:47 -0400

    1. Added storage account 2. Added code to send attachments 3. Addon test console app for local testing of func app

--- infra/sto.tf
+++ infra/sto.tf
@@ -13,6 +13,21 @@ resource "azurerm_storage_container" "func_email_deployment" {
   container_access_type = "private"
 }
 
+resource "azurerm_storage_account" "logs_email" {
+  name                     = format("stmailog%s", substr(local.resource_suffix_lowercase, 0, 14)) # limit suffix to 14 chars
+  resource_group_name      = local.resource_group_name
+  location                 = local.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.tags
+}
+
+resource "azurerm_storage_table" "email_logs" {
+  name                  = "emaillogstore"
+  storage_account_name  = azurerm_storage_account.logs_email.name
+}
+
+
 resource "azurerm_storage_account" "func_sms" {
   name                     = format("stsms%s", local.resource_suffix_lowercase)
   resource_group_name      = local.resource_group_name
